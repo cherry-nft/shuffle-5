cc45c1d120881ab7504d72c0d1d37ce337da618b
cherry-nft <109643299+cherry-nft@users.noreply.github.com>
2024-03-05 05:30:07 +0000

    222

--- components/user-nav.tsx
+++ components/user-nav.tsx
@@ -15,15 +15,9 @@ export function UserNav() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="relative size-[30px] md:flex hidden rounded-full"
-        >
+        <Button variant="ghost" className="relative size-[30px] md:flex hidden rounded-full">
           <Avatar className="size-[30px]">
-            <AvatarImage
-              src="https://avatar.vercel.sh/username"
-              alt="@username"
-            />
+            <AvatarImage src="https://avatar.vercel.sh/username" alt="@username" />
             <AvatarFallback>UN</AvatarFallback>
           </Avatar>
         </Button>
@@ -32,32 +26,18 @@ export function UserNav() {
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
             <p className="text-sm font-medium leading-none">username</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              user@name.com
-            </p>
+            <p className="text-xs leading-none text-muted-foreground">user@name.com</p>
           </div>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem>
-            Profile
-            <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Billing
-            <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            Settings
-            <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-          </DropdownMenuItem>
+          <DropdownMenuItem>Profile<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut></DropdownMenuItem>
+          <DropdownMenuItem>Billing<DropdownMenuShortcut>⌘B</DropdownMenuShortcut></DropdownMenuItem>
+          <DropdownMenuItem>Settings<DropdownMenuShortcut>⌘S</DropdownMenuShortcut></DropdownMenuItem>
           <DropdownMenuItem>New Team</DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          Log out
-          <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
-        </DropdownMenuItem>
+        <DropdownMenuItem>Log out<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut></DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
