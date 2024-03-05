ca92b1c56961f0c1a73fd5d4ff05100ca450f6b7
cherry-nft <109643299+cherry-nft@users.noreply.github.com>
2024-03-05 03:42:14 +0000

    222

---
+++ database.js
@@ -0,0 +1,24 @@
+const { Client } = require('pg')
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+})
+
+async function main() {
+  await client.connect()
+
+  // Create a new users table
+  await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(50),
+      email VARCHAR(50)
+    )
+  `)
+
+  console.log('Users table created.')
+
+  await client.end()
+}
+
+main().catch(console.error)
